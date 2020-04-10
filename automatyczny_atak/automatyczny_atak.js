79de008fbd4ebea27d676bb71993ce26b473e989
Jan Brzeżański <34463932+quetzelcoatlus@users.noreply.github.com>
2020-04-10 18:59:57 +0200

    Update automatyczny_atak.js

--- automatyczny_atak/automatyczny_atak.js
+++ automatyczny_atak/automatyczny_atak.js
@@ -6,11 +6,11 @@
 time = document.querySelector("#date_arrival > span");
 button = document.getElementById("troop_confirm_go");
 
-date="dzisiaj o 15:01:28"   // wprowadź czas dojścia ataku, który pokrywa się z wartością "Przybycie:" w ekranie potwierdzenia ataku
-milis=555                   // wprowadź milisekundę, w której ma wejść atak
-delay=20                    // parametr opóźnienia ataku - do indywidualnej konfiguracji 
+date = "dzisiaj o 15:01:28"   // wprowadź czas dojścia ataku, który pokrywa się z wartością w linii "Przybycie:" w ekranie potwierdzenia ataku
+milis = 555                   // wprowadź milisekundę, w której ma wejść atak
+delay = 20                    // parametr opóźnienia ataku - do indywidualnej konfiguracji 
 
-$('.relative_time').on('DOMSubtreeModified',function(){
+$('.relative_time').on('DOMSubtreeModified', function(){
       if(time.innerText == date){
         m1 = Date.now();
         m2 = m1;
