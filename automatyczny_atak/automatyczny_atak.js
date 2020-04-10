f0d892c12226cfafc91f621c72771d4fb2157297
Jan Brzeżański <34463932+quetzelcoatlus@users.noreply.github.com>
2020-04-10 18:27:50 +0200

    Dodanie skryptu automatycznie wykonującego atak
    Skrypt, który kliknie przycisk wysłania ataku tak, żeby atak doszedł w podanym czasie (wraz z milisekundami).

---
+++ automatyczny_atak/automatyczny_atak.js
@@ -0,0 +1,23 @@
+//var jqry = document.createElement('script');
+//jqry.src = "https://code.jquery.com/jquery-3.3.1.min.js";
+//document.getElementsByTagName('head')[0].appendChild(jqry);
+//jQuery.noConflict();
+
+time = document.querySelector("#date_arrival > span");
+button = document.getElementById("troop_confirm_go");
+
+date="dzisiaj o 15:01:28"   // wprowadź czas dojścia ataku, który pokrywa się z wartością "Przybycie:" w ekranie potwierdzenia ataku
+milis=555                   // wprowadź milisekundę, w której ma wejść atak
+delay=20                    // parametr opóźnienia ataku - do indywidualnej konfiguracji 
+
+$('.relative_time').on('DOMSubtreeModified',function(){
+      if(time.innerText == date){
+        m1 = Date.now();
+        m2 = m1;
+        send = milis+delay;
+        while (m2 - m1 < send)
+            m2 = Date.now();
+
+        button.click();
+    }
+})
