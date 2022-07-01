70c658133fd02ae4c9163a253023cba9fb8e07ad
Jan Brzeżański <34463932+quetzelcoatlus@users.noreply.github.com>
2022-07-01 15:19:21 +0200

    Update asystent_kontynuacji_rownego_zbierania.js

--- asystent_kontynuacji_rownego_zbierania/asystent_kontynuacji_rownego_zbierania.js
+++ asystent_kontynuacji_rownego_zbierania/asystent_kontynuacji_rownego_zbierania.js
@@ -13,9 +13,10 @@ for (var a=0; a<40; a++){
         if(! document.querySelector("#scavenge_screen > div > div.options-container > div:nth-child(" + level + ") > div.status-specific > div > ul > li:nth-child(4) > span.return-countdown")){
             min = 1000;
             choose = level;
-            time = "00:00:01";        
+            time = "00:00:01";   
+            break;
         }
-        var time_left = document.querySelector("#scavenge_screen > div > div.options-container > div:nth-child(" + level + ") > div.status-specific > div > ul > li:nth-child(4) > span.return-countdown").innerHTML
+        var time_left = document.querySelector("#scavenge_screen > div > div.options-container > div:nth-child(" + level + ") > div.status-specific > div > ul > li:nth-child(4) > span.return-countdown").innerHTML;
         let ms = Number(time_left.split(':')[0]) * 60 * 60 * 1000 + Number(time_left.split(':')[1]) * 60 * 1000 + Number(time_left.split(':')[2]) * 1000 + 5000;
         if (ms < min){
             min = ms;
@@ -24,13 +25,13 @@ for (var a=0; a<40; a++){
         }
     }
     
-    console.log("Waiting " + time + " (" + min + " miliseconds) for level " + choose + " scavenge. [" + new Date().toLocaleString() + "]")
-    await mysleep(min)
+    console.log("Waiting " + time + " (" + min + " miliseconds) for level " + choose + " scavenge. [" + new Date().toLocaleString() + "]");
+    await mysleep(min);
     
-    console.log("Selecting all troops")
-    document.querySelector("#scavenge_screen > div > div.candidate-squad-container > table > tbody > tr:nth-child(2) > td.squad-village-required > a").click()
-    await mysleep(1000)
-    console.log("Sending troops for " + choose + " level scavenge.")
-    document.querySelector("#scavenge_screen > div > div.options-container > div:nth-child(" + choose + ") > div.status-specific > div > div.action-container > a.btn.btn-default.free_send_button").click()
-    await mysleep(1000)
+    console.log("Selecting all troops");
+    document.querySelector("#scavenge_screen > div > div.candidate-squad-container > table > tbody > tr:nth-child(2) > td.squad-village-required > a").click();
+    await mysleep(1000);
+    console.log("Sending troops for " + choose + " level scavenge.");
+    document.querySelector("#scavenge_screen > div > div.options-container > div:nth-child(" + choose + ") > div.status-specific > div > div.action-container > a.btn.btn-default.free_send_button").click();
+    await mysleep(1000);
 }
