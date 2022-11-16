076323d5006469075583b3380a65a817b54e4721
Jan Brzeżański <34463932+quetzelcoatlus@users.noreply.github.com>
2022-11-16 17:17:15 +0100

    Update asystent_masowego_zbieracza.js

--- asystent_masowego_zbieracza/asystent_masowego_zbieracza.js
+++ asystent_masowego_zbieracza/asystent_masowego_zbieracza.js
@@ -2,6 +2,7 @@ var units = [
     ["spear", 750, 300, 150, 100, 1]
 ];
 
+var time_left = "0:00:01"
 
 function mysleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -19,22 +20,24 @@ function fill(unit, number) {
 }
 
 for(var k=0; k<10; k++){
+    let ms = Number(time_left.split(':')[0]) * 60 * 60 * 1000 + Number(time_left.split(':')[1]) * 60 * 1000 + Number(time_left.split(':')[2]) * 1000 + 5000;
+    console.log("Waiting " + time_left + " (" + ms + " miliseconds).");
+    await mysleep(ms);
+    
     for(var i=4; i>0; i--){
         for(var j=0; j<units.length; j++){
             fill(units[j][0], units[j][i] * units[j][5])
-            await mysleep(500);
+            await mysleep(300);
         }
         document.querySelector("#scavenge_mass_screen > div > div.villages-container > table > tbody > tr:nth-child(1) > td:nth-child("+(i+1)+") > input").click()
         await mysleep(500);
         document.querySelector("#scavenge_mass_screen > div > div:nth-child(3) > div.buttons-container > a.btn.btn-default.btn-send").click()
-        await mysleep(1000);
+        await mysleep(300);
     }
+    
     for(var j=0; j<units.length; j++){
         fill(units[j][0], units[j][4] * units[j][5])
-        await mysleep(500);
+        await mysleep(300);
     }
     var time_left = document.querySelector("#scavenge_mass_screen > div > div.options-container > div:nth-child(4) > div.status-specific > div > div.squad-preview > ul > li.duration-section > span.duration").innerHTML;
-    let ms = Number(time_left.split(':')[0]) * 60 * 60 * 1000 + Number(time_left.split(':')[1]) * 60 * 1000 + Number(time_left.split(':')[2]) * 1000 + 5000;
-    console.log("Waiting " + time_left + " (" + ms + " miliseconds).");
-    await mysleep(ms);
 }
